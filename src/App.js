2aa8bf74b73a3061dce27cc8b4366022c2f080f3
RaeesaParker <raeesaparker101@gmail.com>
2022-11-03 11:58:27 +0000

    Add footer component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div >
+      <Footer></Footer>
     </div>
   );
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,21 @@
 import React from "react";
 
 function Footer() {
+
+  // Create variable for companyName
+  const companyName = "Cats4Lyf"
+
+  // Create variable for date
+  const newDate = new Date;
+  const currentYear = newDate.getFullYear();
+
   return (
-    <div >
+    <div>
+      <footer className="footer">
+        <div className="footer-container">
+          <a>&copy; {companyName} {currentYear}</a>
+        </div>
+      </footer>
     </div>
   );
 }

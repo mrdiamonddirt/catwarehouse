cc5036e443ab20e16d90fdf0de18d6f1b0f8d12b
RaeesaParker <raeesaparker101@gmail.com>
2022-11-03 14:24:21 +0000

    Add state for cat basket

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Navbar from "./components/Navbar";
 import './App.css';
 import Footer from './components/Footer';
@@ -5,6 +6,12 @@ import {Home} from './components/Home'
 
 
 function App() {
+
+  // State to store the array of the basket 
+  // Needed here so we can pass the data to both the header and the main section
+  const [basket, setBasket] = useState([])
+
+
   return (
 
     <div >

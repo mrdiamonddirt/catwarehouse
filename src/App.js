7f82d82bd3b869f550ed9054ff29ba00b723d36d
RowDog <mrdiamonddirt@gmail.com>
2022-11-03 14:47:29 +0000

    catdata and prices from api

--- src/App.js
+++ src/App.js
@@ -2,9 +2,33 @@ import Navbar from "./components/Navbar";
 import './App.css';
 import Footer from './components/Footer';
 import {Home} from './components/Home'
+import { useEffect , useState } from "react";
+import { faker } from "@faker-js/faker";
 
 
 function App() {
+const [cats, setCats] = useState([])
+
+// useEffect for calling api
+useEffect(() => {
+async function getCats() {
+try {
+  const catData = await fetch('https://api.thecatapi.com/v1/images/search?limit=10')
+  const newdata = await catData.json()
+  const catArray = newdata.map((cat) => {
+    return{
+            catid: cat.id,
+            catimage: cat.url,
+            price: faker.commerce.price(100, 200),
+          };
+  })
+  setCats(catArray)
+} catch (error) {
+  
+}} 
+getCats() 
+}, [])
+
   return (
 
     <div >

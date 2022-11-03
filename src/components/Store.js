ad2d8c006cdb0820f1e929cf088c028e047477e7
RaeesaParker <raeesaparker101@gmail.com>
2022-11-03 14:23:07 +0000

    Add state for cat inventory

--- src/components/Store.js
+++ src/components/Store.js
@@ -1,7 +1,21 @@
-import React from "react";
+import {useState} from "react";
 
 
 function Store() {
+
+  // State to store the array of the cat items 
+  const [catInventory, setCatInventory] = useState([])
+
+
+
+
+  // Will be used to render componants => either inventory or inventory + basket modal 
+  return(
+    <div id="section-store-">
+
+    </div>
+  )
+
 
 }
 

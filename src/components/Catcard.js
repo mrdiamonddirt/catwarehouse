8ec9e50c4e14fc6db513eab4d8b0a4c1da7bd936
RowDog <mrdiamonddirt@gmail.com>
2022-11-03 18:31:55 +0000

    styled cards a little

--- src/components/Catcard.js
+++ src/components/Catcard.js
@@ -5,15 +5,17 @@ const Catcard = (props) => {
     // renders each cat and passes props to each
     return(
         <>
-        <CatcardWrapper>{props.catsdata.map((catObject) => {
+        <div className='CatCards'>
+        <CatcardWrapper >{props.catsdata.map((catObject) => {
             return(
                 <div className='cat'>
                 <img alt='catimage' src={catObject.catimage}></img>
                 <p>{catObject.catid}</p>
                 <p>£{catObject.price}</p>
-                </div>            
+                </div>           
                 )
         })}</CatcardWrapper>
+        </div>
         </>
     )
 }
@@ -22,11 +24,21 @@ export default Catcard;
 
 const CatcardWrapper = styled.div`
 display:flex;
+justify-content: center;
+align-items: center;
+text-align: center;
+/* background-color: aqua; */
+height: 80vh;
 .cat{
+    flex-direction: column;
     margin: 5px;
-}
+    padding: 2px;
+    border: 1px solid black;
+    border-radius: 5px;
 
+}
 img{
+    border-radius: 5px;
     height: 100px;
 }
 `
